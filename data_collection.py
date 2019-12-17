31dc031027a11002218957f1c969f4d16725b3b1
Hong <hxyue1@gmail.com>
2019-12-16 21:06:59 -0800

    scraping stats

--- data_collection.py
+++ data_collection.py
@@ -5,23 +5,15 @@ import pandas as pd
 import csv
 import rankings_scrape as rs
 from rankings_scrape import rankings_scraper, overview_scraper
+import rankings_scrape as rs
 import seaborn as sns
 
 top_100 = rankings_scraper()
 
-top_100['Website'] = 'https://www.atptour.com/' + top_100['Website']
-
-personal_sites = top_100['Website']
-
 overview_df = overview_scraper(top_100['Website'])
 
-####Making some plots
+###Collating stats for Nadal
 
-sns.distplot(top_100['Height'])
-sns.distplot(top_100['Weight'])
-sns.distplot(top_100['Turned_pro'])
-
-sns.scatterplot(top_100['Height'],top_100['Ranking'])
-sns.scatterplot(top_100['Age'],top_100['Ranking'])
-sns.scatterplot(top_100['Height'],top_100['Weight'])    
+url = 'https://www.atptour.com/en/players/rafael-nadal/n409/overview'
+url = re.sub('overview',''.url)
 
